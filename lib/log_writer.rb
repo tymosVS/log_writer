3a86868e7ec18834212ae93b3da795386af8e7c6
Vladimir <timos9vs@gmail.com>
2020-01-10 15:44:54 +0200

    redy base functional

--- lib/log_writer.rb
+++ lib/log_writer.rb
@@ -1,8 +1,6 @@
 require_relative "log_writer/writer"
-# require_relative "log_writer/version"
+require_relative "log_writer/version"
 
 module LogWriter
   class Error < StandardError; end
 end
-
-LogWriter::Writer.new
