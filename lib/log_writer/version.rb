1e934967c89fba6d2cb426e8ce9402eaa901a8ba
Vladimir <timos9vs@gmail.com>
2020-01-13 11:14:45 +0200

    Add tests

--- lib/log_writer/version.rb
+++ lib/log_writer/version.rb
@@ -1,3 +1,3 @@
 module LogWriter
-  VERSION = "0.1.0"
+  VERSION = '0.1.0'.freeze
 end
--- spec/log_writer_spec.rb
+++ spec/log_writer_spec.rb
@@ -1,9 +1,20 @@
 RSpec.describe LogWriter do
-  it "has a version number" do
+  it 'has a version number' do
     expect(LogWriter::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  it 'does something useful' do
+    expect(LogWriter::Writer.new.run).to eq(nil)
+  end
+
+  it 'print default message' do
+    str = "Some text\n"
+    expect{ LogWriter::Writer.new.run }.to output(str).to_stdout
+  end
+
+  it 'Check change printed text' do
+    l_w = LogWriter::Writer.new
+    l_w.default_string = 'Check text'
+    expect{ l_w.run }.to output("Check text\n").to_stdout
   end
 end
