ca5044d3bab3d171f6643c4bd64a72786aff2f19
Vladimir <timos9vs@gmail.com>
2020-01-13 11:42:27 +0200

    clearing tests and add symbolic pictures

--- spec/log_writer_spec.rb
+++ spec/log_writer_spec.rb
@@ -1,4 +1,29 @@
 RSpec.describe LogWriter do
+  let(:img) {
+    '
+      ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+      ░░░░░░░░░░░░░▄▄▄▄▄▄▄░░░░░░░░░
+      ░░░░░░░░░▄▀▀▀░░░░░░░▀▄░░░░░░░
+      ░░░░░░░▄▀░░░░░░░░░░░░▀▄░░░░░░
+      ░░░░░░▄▀░░░░░░░░░░▄▀▀▄▀▄░░░░░
+      ░░░░▄▀░░░░░░░░░░▄▀░░██▄▀▄░░░░
+      ░░░▄▀░░▄▀▀▀▄░░░░█░░░▀▀░█▀▄░░░
+      ░░░█░░█▄▄░░░█░░░▀▄░░░░░▐░█░░░
+      ░░▐▌░░█▀▀░░▄▀░░░░░▀▄▄▄▄▀░░█░░
+      ░░▐▌░░█░░░▄▀░░░░░░░░░░░░░░█░░
+      ░░▐▌░░░▀▀▀░░░░░░░░░░░░░░░░▐▌░
+      ░░▐▌░░░░░░░░░░░░░░░▄░░░░░░▐▌░
+      ░░▐▌░░░░░░░░░▄░░░░░█░░░░░░▐▌░
+      ░░░█░░░░░░░░░▀█▄░░▄█░░░░░░▐▌░
+      ░░░▐▌░░░░░░░░░░▀▀▀▀░░░░░░░▐▌░
+      ░░░░█░░░░░░░░░░░░░░░░░░░░░█░░
+      ░░░░▐▌▀▄░░░░░░░░░░░░░░░░░▐▌░░
+      ░░░░░█░░▀░░░░░░░░░░░░░░░░▀░░░
+      ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+    '
+  }
+  let(:log_writer) { LogWriter::Writer.new }
+
   it 'has a version number' do
     expect(LogWriter::VERSION).not_to be nil
   end
@@ -13,13 +38,22 @@ RSpec.describe LogWriter do
   end
 
   it 'Check change printed text' do
-    l_w = LogWriter::Writer.new
-    l_w.default_string = 'Check text'
-    expect{ l_w.run }.to output("Check text\n").to_stdout
+    log_writer.default_string = 'Check text'
+    expect{ log_writer.run }.to output("Check text\n").to_stdout
   end
 
   it 'Possible set new default string using inisializer' do
     l_w = LogWriter::Writer.new('Check text')
     expect{ l_w.run }.to output("Check text\n").to_stdout
+  end
+
+  it 'Possible set symbolic picture using inisializer' do
+    l_w = LogWriter::Writer.new(img)
+    expect{ l_w.run }.to output(img + "\n").to_stdout
+  end
+
+  it 'possible set symbolic picture using setter' do
+    log_writer.default_string = img
+    expect{ log_writer.run }.to output(img + "\n").to_stdout
   end
 end
