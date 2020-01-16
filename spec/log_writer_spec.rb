46949cc0f960aacb34a4ac8d5cc4e3a0447e0f49
Vladimir <timos9vs@gmail.com>
2020-01-16 11:38:11 +0200

    Fix tests

--- spec/log_writer_spec.rb
+++ spec/log_writer_spec.rb
@@ -28,31 +28,27 @@ RSpec.describe LogWriter do
     expect(LogWriter::VERSION).not_to be nil
   end
 
-  it 'does something useful' do
+  it 'do not return someting' do
     expect(LogWriter::Writer.new.run).to eq(nil)
   end
 
   it 'print default message' do
-    expect{ LogWriter::Writer.new.run }.to output(str).to_stdout
+    str = 'Some text'
+    expect { LogWriter::Writer.new.run }.to output(str + "\n").to_stdout
   end
 
   it 'Check change printed text' do
-    log_writer.default_string = 'Check text'
-    expect{ log_writer.run }.to output("Check text\n").to_stdout
-  end
-
-  it 'Possible set new default string using inisializer' do
-    l_w = LogWriter::Writer.new('Check text')
-    expect{ l_w.run }.to output("Check text\n").to_stdout
-  end
-
-  it 'Possible set symbolic picture using inisializer' do
-    l_w = LogWriter::Writer.new(img)
-    expect{ l_w.run }.to output(img + "\n").to_stdout
+    str = 'Check text'
+    LogWriter.configure do |config|
+      config.default_text = str
+    end
+    expect { log_writer.run }.to output("Check text\n").to_stdout
   end
 
   it 'possible set symbolic picture using setter' do
-    log_writer.default_string = img
+    LogWriter.configure do |config|
+      config.default_text = img
+    end
     expect{ log_writer.run }.to output(img + "\n").to_stdout
   end
 end
